29669f13e4c99eed7b9a67cd1b1b8d8a58884220
stanleytangerror <tangshunlei@gmail.com>
2016-07-28 17:49:55 +0800

    refactor one to one

--- connect.js
+++ connect.js
@@ -1,57 +1,106 @@
-// Create a new Peer with our demo API key, with debug set to true so we can
-// see what's going on.
-var peer = new Peer({
-    key: '0kl9mk3ed0gj5rk9',
-    debug: 3
-});
-
-// The `open` event signifies that the Peer is ready to connect with other
-// Peers and, if we didn't provide the Peer with an ID, that an ID has been
-// assigned by the server.
-peer.on('open', function(id) {
-    $('#local-id').val(id);
-});
-
-    // Emitted when a new data connection is established from a remote peer.
-peer.on('connection', function (connection) {
-    conn = connection;
-    initDataConnection(conn);
-});
-
-var conn;
-
-function initDataConnection(connection) {
-    connection.on('open', function() {
-        print('connection established');
-        $('#remote-id').val(connection.peer);
-    });
-    // Receive messages
-    connection.on('data', function(data) {
-        print('receive: ' + data);
-        drawCallback(data);
-    });
-}
-
 //////////////////////////
-// start connection
+// p2p class prototype
 //////////////////////////
-
-function startConnect() {
-    conn = peer.connect($('#remote-id').val());
+P2P = function (serverKey, 
+        eventPeerOpen, eventTriggleConnect, eventEmitConnect, 
+        eventReceiveData, eventSendData) {
+    this.peer = new Peer({key: serverKey, debug: 3});
+    this.conn = undefined;
+    this.localId = undefined;
+    this.remoteId = undefined;
     
-    initDataConnection(conn);
+    this.eventPeerOpen = eventPeerOpen;
+    this.eventTriggleConnect = eventTriggleConnect;
+    this.eventEmitConnect = eventEmitConnect;
+    this.eventReceiveData = eventReceiveData;
+    this.eventSendData = eventSendData;
+    
+    var P2PObject = this;
+    
+    // on create local peer
+    P2PObject.peer.on('open', function(id) {
+        P2PObject.localId = id;
+        P2PObject.eventPeerOpen(P2PObject);
+    });
+
+    // on receiving a remote peer connection require
+    P2PObject.peer.on('connection', function (connection) {
+        P2PObject.conn = connection;
+        
+        // on connection open
+        P2PObject.conn.on('open', function() {
+            P2PObject.remoteId = P2PObject.conn.peer;
+            P2PObject.eventTriggleConnect(P2PObject);
+        });
+        
+        // on receiving data
+        P2PObject.conn.on('data', function(data) {
+            P2PObject.eventReceiveData(P2PObject, data);
+        });
+    });
 }
 
-function sendData(data) {
-    send($('#send-data').val()); 
-    $('#send-data').val('');
+P2P.prototype = {
+    // on sending a remoite peer connection require
+    connect : function (remotePeerId) {
+        this.conn = this.peer.connect(remotePeerId);
+        
+        var P2PObject = this;
+        
+        // on connection open
+        this.conn.on('open', function() {
+            P2PObject.remoteId = P2PObject.conn.peer;
+            P2PObject.eventEmitConnect(this);
+        });
+        
+        // on receiving data
+        this.conn.on('data', function(data) {
+            P2PObject.eventReceiveData(P2PObject, data);
+        });
+    },
+    
+    // on sending data
+    send : function(data) {
+        this.conn.send(data);
+        this.eventSendData(this, data);
+    }
+};
+
+
+//////////////////////////
+// utils
+//////////////////////////
+
+var p2p = new P2P('0kl9mk3ed0gj5rk9', 
+    function(p2p) {
+        $('#local-id').val(p2p.localId);
+    },
+    function(p2p) {
+        print('connection established');
+        $('#remote-id').val(p2p.remoteId);        
+    },
+    function() {
+        print('connection established');
+    },
+    function(p2p, data) {
+        print(p2p.remoteId + ': ' + data);
+        drawCallback(data);        
+    },
+    function(p2p, data) { 
+        $('#send-data').val('');
+        print(p2p.localId + ': ' + data);
+    }    
+);
+
+function sendData() {
+    var data = $('#send-data').val();
+    p2p.send(data);
 }
 
-function send(data) {
-    conn.send(data);
-    print('send: ' + data)
+function connectPeer() {
+    var remoteid = $('#remote-id').val();
+    p2p.connect(remoteid);
 }
-
 
 //////////////////////////
 // web console
--- draw.js
+++ draw.js
@@ -114,13 +114,13 @@ canvas.onmousedown = function(e) {
     // myPencil.lastX = e.pageX - this.offsetLeft;
     // myPencil.lastY = e.pageY - this.offsetTop;
     var msg = new Message('start', e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    send(JSON.stringify(msg));
+    p2p.send(JSON.stringify(msg));
     drawStart(ctx, myPencil, msg);
 };
 
 canvas.onmouseup = function(e){
     var msg = new Message('end', e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    send(JSON.stringify(msg));
+    p2p.send(JSON.stringify(msg));
     drawEnd(ctx, myPencil);
 }
 
@@ -128,7 +128,7 @@ canvas.onmousemove = function(e) {
     if (!myPencil.painting) return;
     
     var msg = new Message('draw', e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    send(JSON.stringify(msg));
+    p2p.send(JSON.stringify(msg));
     drawOperate(ctx, myPencil, msg);
 
 }
